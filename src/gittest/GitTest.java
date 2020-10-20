2124a2b5494e2c3faef0225b52efb0e3b9966815
Suppawit Wiriyapuwadol <suppawit.41218@gmail.com>
2020-10-20 15:36:30 +0700

    First commit

---
+++ src/gittest/GitTest.java
@@ -0,0 +1,14 @@
+package gittest;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        SayHello();
+
+    }
+
+    public static void SayHello() {
+        System.out.println("Helloooooooooooo");
+    }
+
+}
